6d685d99c0330d3bcdaec6e3961b2520b94e7f46
Irfan545 <controlaltdefeat2@gmail.com>
2025-04-24 01:19:09 +0100

    Fix framer-motion resolution issue

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,8 @@ import path from "path";
 const nextConfig: NextConfig = {
   transpilePackages: ["framer-motion"],
   webpack: (config) => {
-    config.resolve.alias["framer-motion"] = path.resolve(
-      __dirname,
-      "node_modules/framer-motion/dist/framer-motion.cjs"
-    );
+    config.resolve.alias["framer-motion"] =
+      "framer-motion/dist/framer-motion.cjs";
     return config;
   },
 };
